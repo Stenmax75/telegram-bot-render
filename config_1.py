c2bd627cebbede265ea5cfe04dff5ab850edbe02
Stenmax75 <167688114+Stenmax75@users.noreply.github.com>
2025-11-01 12:08:17 +0200

    Update environment variable handling in config_1.py

--- config_1.py
+++ config_1.py
@@ -9,7 +9,7 @@ load_dotenv()
 # Telegram
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 REQUIRED_CHANNEL_ID = os.getenv("REQUIRED_CHANNEL_ID")
-WEBHOOK_SECRET = os.getenv("WEBHOOK_SECRET", "default_secret_key_change_me") 
+WEBHOOK_SECRET = os.getenv("RENDER_EXTERNAL_URL", "default_secret_key_change_me") 
 
 # Database (MySQL/TiDB)
 # TiDB требует эти переменные, которые мы настроим на Render
@@ -27,4 +27,5 @@ BASE_WEBHOOK_URL = os.getenv("BASE_WEBHOOK_URL")
 # Проверка на наличие критически важных данных
 if not (BOT_TOKEN and BASE_WEBHOOK_URL and DB_USER and DB_HOST and DB_NAME):
     # Добавили DB_PORT в условие
+
     raise ValueError("Одна или несколько критически важных переменных (BOT_TOKEN, BASE_WEBHOOK_URL, DB_USER, DB_HOST, DB_NAME, DB_PORT) не найдены.")
